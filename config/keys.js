b6136a1278d3a2f9542a2eaa0bf1229b401b704d
Avoy Baruah <tinkuagarwalg@gmail.com>
2025-02-23 14:09:16 +0530

    profile done

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports ={
-    mongoURI:'mongodb+srv://avoyisgay2:gayisavoy2@cluster0.digeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-}
+    mongoURI:'mongodb+srv://avoyisgay2:gayisavoy2@cluster0.digeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    secretOrKey: 'secret'
+};
